562d1b2535b9d5c5ccc4e1ee698f699b5e3296b3
Lucas Henrique Pereira Alves <202506088201@alunos.estacio.br>
2025-08-26 17:21:52 +0000

    DESAFIO MOVIMENTACAO PECAS DE XADREZ NIVEL INICIANTE

--- xadrez.c
+++ xadrez.c
@@ -4,24 +4,31 @@ int main () {
 int  r = 0, b = 0;
 
 // MOVIMENTAÇÃO DA TORRE
-printf("Vamos simular a movimentação da torre para cima no tabuleiro.\n");
+printf("TORRE 5 CASAS PRA FRENTE.\n");
 
-for (int t = 0; t <= 5; t++){
-    printf("CIMA.\n");
+for (int t = 0; t < 5; t++){
+    printf("FRENTE.\n");
 }
 
 //MOVIMENTAÇÃO DA RAINHA
 
-printf("Vamo fazer a rainha andar para a direita.\n");
+printf("RAINHA 8 CASAS PRA DIREITA.\n");
 
-while (r <= 8){
+while (r < 8){
 
-printf("RAINHA OITO CASAS PARA DIREITA.\n");
+printf("DIREITA.\n");
 
 r++;
     } 
 
-    
+printf("BISPO 5 CASAS DIAGONAL PARA DIREITA. (PARA CIMA E DIREITA).\n");
+
+    do {
+
+        printf("CIMA DIREITA.\n");
+        b++;
+
+    } while (b < 5);
 
     return 0;
 }
