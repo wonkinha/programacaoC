34921580da1b65c0c4e9f06c92698be82d74f31a
Lucas Henrique Pereira Alves <202506088201@alunos.estacio.br>
2025-08-26 17:13:27 +0000

    DESAFIO MOVIMENTAÇÃO PEÇAS XADREZ NIVEL INICIANTE

---
+++ xadrez.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main () {
+
+int  r = 0, b = 0;
+
+// MOVIMENTAÇÃO DA TORRE
+printf("Vamos simular a movimentação da torre para cima no tabuleiro.\n");
+
+for (int t = 0; t <= 5; t++){
+    printf("CIMA.\n");
+}
+
+//MOVIMENTAÇÃO DA RAINHA
+
+printf("Vamo fazer a rainha andar para a direita.\n");
+
+while (r <= 8){
+
+printf("RAINHA OITO CASAS PARA DIREITA.\n");
+
+r++;
+    } 
+
+    
+
+    return 0;
+}
